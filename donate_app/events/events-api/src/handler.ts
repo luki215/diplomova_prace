3e7c97ccf30f4f1dcbcd5f0fa74419288b4129a1
Lukas Brezina <lukas@brezinovi.sk>
2020-12-08 13:36:54 +0100

    feat(api): show only future events

--- donate_app/events/events-api/src/handler.ts
+++ donate_app/events/events-api/src/handler.ts
@@ -5,11 +5,20 @@ declare global {
 export async function handleRequest(request: Request): Promise<Response> {
   const keys = await EVENTS_KV.list()
   console.log(keys.keys)
-  const events = await Promise.all(
-    keys.keys.map((key) =>
-      EVENTS_KV.get(key.name).then((x) => (x ? JSON.parse(x) : x)),
-    ),
-  )
+  const events = (
+    await Promise.all(
+      keys.keys.map((key) =>
+        EVENTS_KV.get(key.name).then((x) => (x ? JSON.parse(x) : x)),
+      ),
+    )
+  ).filter((e) => {
+    const date = e.date.split(',')[0]
+    const dateSplit = date.split('. ')
+    const dateDate = new Date(+dateSplit[2], +dateSplit[1] - 1, +dateSplit[0])
+
+    return dateDate.getTime() > new Date().getTime()
+  })
+
   return new Response(JSON.stringify(events, null, 2), {
     headers: {
       'content-type': 'application/json;charset=UTF-8',
