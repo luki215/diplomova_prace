06136b7f5521865644e57fad542189b18b7a8c75
Lukas Brezina <lukas@brezinovi.sk>
2021-03-24 11:11:51 +0100

    feat(eshop): reinit to new nestjs

--- eshop/backend/src/app.module.ts
+++ eshop/backend/src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GoodsModule } from './goods/goods.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-    }),
-    GoodsModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
