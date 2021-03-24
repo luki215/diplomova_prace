52cf3ff07ff26e80d34889e228c909c176e56589
Lukas Brezina <lukas@brezinovi.sk>
2021-03-24 16:02:30 +0100

    feat(eshop-fe): header kick-off

--- eshop/clients/angular/src/app/app-routing.module.ts
+++ eshop/clients/angular/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) }, { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
