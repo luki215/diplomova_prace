5d3f91f0d3c57a442bbdc4bd4ad4ff6d8374127b
Lukas Brezina <lukas@brezinovi.sk>
2021-03-24 19:08:22 +0100

    categories page

--- eshop/clients/angular/src/app/app-routing.module.ts
+++ eshop/clients/angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) }, { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) }, { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
+  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: 'cat', loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
