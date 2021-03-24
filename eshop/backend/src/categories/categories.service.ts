db69d474293da11f40cd8310a3683449088cc061
Lukas Brezina <lukas@brezinovi.sk>
2021-03-24 18:57:37 +0100

    feat(eshop-be): fix categories service data source

--- eshop/backend/src/categories/categories.service.ts
+++ eshop/backend/src/categories/categories.service.ts
@@ -1,20 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Observable, of } from 'rxjs';
-// import { categories } from './categories.data';
+import { categories } from './categories.data';
 import { Category } from '../shared/interfaces/categories.interfaces';
-
-const categories: Category[] = [
-  {
-    name: "Trvalky",
-    slug: "trvalky",
-    background: `${process.env.HOST}/images/cat/cat1.jpg`,
-    seo: {
-      title: "Trvalky | Zahradnictví Březina",
-      description: "Rostliny, které žijí, dokud je nezapomenete dlouho zalít"
-    }
-  }
-]
-
 
 @Injectable()
 export class CategoriesService {
@@ -24,7 +11,7 @@ export class CategoriesService {
   }
 
   find(slug: string): Observable<Category> | void {
-    // const cat = categories.find(x => x.slug === slug)
-    // return of(cat)
+    const cat = categories.find(x => x.slug === slug)
+    return of(cat)
   }
 }
