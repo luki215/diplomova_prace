83e721903e52a99fc6282b6d516a93853aea4cc0
Lukas Brezina <lukas@brezinovi.sk>
2021-03-24 19:25:42 +0100

    fix(eshop-be) fix category detail

--- eshop/backend/src/categories/categories.controller.ts
+++ eshop/backend/src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 
 @Controller('categories')
@@ -11,7 +11,7 @@ export class CategoriesController {
   }
 
   @Get(':slug')
-  find(slug: string) {
+  find(@Param('slug') slug: string) {
     return this.categoriesService.find(slug);
   }
 }
