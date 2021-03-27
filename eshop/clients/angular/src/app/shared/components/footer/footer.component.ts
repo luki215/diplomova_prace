9f3bc127e0df98df2b3be68a75264bf6b3509a91
Lukas Brezina <lukas@brezinovi.sk>
2021-03-27 11:40:29 +0100

    feat(eshop-fe): footer

--- eshop/clients/angular/src/app/shared/components/footer/footer.component.ts
+++ eshop/clients/angular/src/app/shared/components/footer/footer.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SiteInfo } from '../../interfaces/site-info.interfaces';
+import { SiteInfoService } from '../../services/site-info.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public siteInfo$: Observable<SiteInfo>;
+  public days = [
+    'Pondělí',
+    'Úterý',
+    'Středa',
+    'Čtvrtek',
+    'Pátek',
+    'Sobota',
+    'Neděle',
+  ];
+  constructor(private siteInfoService: SiteInfoService) {
+    this.siteInfo$ = this.siteInfoService.get();
   }
 
+  ngOnInit(): void {}
 }
